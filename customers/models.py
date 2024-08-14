595f8be420f93a1e8462bb45a73aa43dcfc2c65a
Jayakrishnanpdm <jayakrishnanmanupdm@gmail.com>
2024-08-14 19:57:51 +0530

    app_design

---
+++ customers/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Customer(models.Model):
+    address=models.TextField()
+    phone=models.IntegerField()
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    title=models.CharField(max_length=20)
+    price=models.IntegerField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='\images')
+    priority=models.IntegerField(default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
