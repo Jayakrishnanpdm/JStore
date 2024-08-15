f01ce821446e55913037ccb95996dcfeda4ce065
Jayakrishnanpdm <jayakrishnanmanupdm@gmail.com>
2024-08-15 17:25:34 +0530

    final_template_design

--- products/urls.py
+++ products/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+   
+    path('home/', views.home),
+    path('products/', views.products),
+    path('product_detail/', views.product_detail),
+    path('cart/', views.cart),
+    path('account/', views.account),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
     return render(request,'home_layout.html')
+
+def products(request):
+    return render(request,'products.html')
+
+def product_detail(request):
+    return render(request,'home_layout.html')
+
+def product_detail(request):
+    return render(request,'product_detail.html')
+
+def account(request):
+    return render(request,'account.html')
+
+def cart(request):
+    return render(request,'cart.html')
