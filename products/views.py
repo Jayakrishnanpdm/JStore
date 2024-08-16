285ea93e1d4956871afa97f326e684710fcc78c9
Jayakrishnanpdm <jayakrishnanmanupdm@gmail.com>
2024-08-16 23:52:05 +0530

    final_product_design

--- products/views.py
+++ products/views.py
@@ -7,10 +7,13 @@ def home(request):
     return render(request,'home_layout.html')
 
 def products(request):
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1)    
     product_list=Products.objects.all()
     paginator=Paginator(product_list,3)
-    page=paginator.get_page(1)
-    return render(request,'products.html',{"product_list":page})
+    page_list=paginator.get_page(page)
+    return render(request,'products.html',{"product_list":page_list})
 
 
 def product_detail(request):
